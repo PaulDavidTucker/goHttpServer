83c389060b9b22590f37335f6cb932687a6696fe
PaulDavidTucker <tuckerpauldavid@gmail.com>
2024-10-23 20:26:11 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+// Hello world!
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println("Starting the web server! Visit http://localhost:8080/quote to get a quote.")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.HandleFunc("/quote", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, quote.Go())
+	})
+
+	http.ListenAndServe(":8080", nil)
+
+}
